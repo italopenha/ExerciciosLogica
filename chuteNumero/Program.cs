354286a92c6cf85e55fed1a7546bdcdd609be0c4
italopenha <italopenha77@outlook.com>
2022-11-22 16:51:22 -0300

    Adicionando exercícios

---
+++ chuteNumero/Program.cs
@@ -0,0 +1,25 @@
+﻿Random valorAleatorio = new Random();
+
+int valorInteiro = valorAleatorio.Next(1, 10);
+int chuteUsuario;
+bool funcionando = true;
+
+while (funcionando)
+{
+    Console.Write("Digite um valor de 1 a 10: ");
+    chuteUsuario = int.Parse(Console.ReadLine());
+
+    if (chuteUsuario > valorInteiro)
+    {
+        Console.WriteLine("Valor maior! Digite um número menor do que " + chuteUsuario);
+    }
+    else if (chuteUsuario < valorInteiro)
+    {
+        Console.WriteLine("Valor menor! Digite um número maior do que " + chuteUsuario);
+    }
+    else if (chuteUsuario == valorInteiro)
+    {
+        Console.Write("Acertou!");
+        funcionando = false;
+    }
+}
---
+++ salarioPorHora/Program.cs
@@ -0,0 +1,13 @@
+﻿double SalarioMensal;
+double HorasPorMes;
+double SalarioPorHora;
+
+Console.Write("Qual o seu salário mensal? ");
+SalarioMensal = double.Parse(Console.ReadLine());
+
+Console.Write("Quantas horas você trabalha por mês? ");
+HorasPorMes = double.Parse(Console.ReadLine());
+
+SalarioPorHora = SalarioMensal / HorasPorMes;
+
+Console.WriteLine("O seu salário por horas é igual a: " + SalarioPorHora);
